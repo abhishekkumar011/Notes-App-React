1c28c916dd7b015de04686406c9fee1b9b2065bc
Abhishek <abhishekkumar0336165@gmail.com>
2023-01-27 21:43:08 +0530

    Add and save note featured added

--- src/App.js
+++ src/App.js
@@ -29,9 +29,21 @@ function App() {
     }
   ]);
 
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString(),
+    }
+
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  }
+
   return (
     <div className='container'>
-      <NotesList notes={notes}/>
+      <NotesList notes={notes} handleAddNote={addNote}/>
     </div>
   );
 }
--- src/Components/NotesList.js
+++ src/Components/NotesList.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import AddNote from './AddNote';
 import Notes from './Notes'
 
-function NotesList({ notes }) {
+function NotesList({ notes, handleAddNote }) {
   return (
     <div className="notes-list">
       {notes.map((note) =>
         <Notes id={note.id} text={note.text} date={note.date}/>
       )}
+      <AddNote handleAddNote={handleAddNote}/>
     </div>
   );
 }
